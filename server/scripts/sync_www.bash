af1fb7da24b7cd364a729a38078c573c3e05646e
Stephen <brawner@gmail.com>
2014-07-04 14:07:35 -0400

    updating scripts

--- server/scripts/sync_www.bash
+++ server/scripts/sync_www.bash
@@ -4,5 +4,5 @@ mkdir -p /opt/robocook/server
 rsync -av --exclude '.git' ../ /opt/robocook/server/
 
 mkdir -p /var/www/robocook/
-rsync -av --exclude 'server' --exclude '.git'  ../../ /var/www/robocook/
+rsync -av --exclude 'server' --exclude '.git'  ../../web/ /var/www/robocook/
 chmod 775 -R /var/www/robocook
