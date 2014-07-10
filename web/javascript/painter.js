f0309bebba3300e244edd39e11b35e0c61b7a342
Stephen <brawner@gmail.com>
2014-07-10 14:19:46 -0400

    address issue #10

--- web/javascript/painter.js
+++ web/javascript/painter.js
@@ -582,23 +582,42 @@ var RecipePainter = function() {
 
 };
 
+$.fn.textHeight = function()
+ {
+   var self = $(this),
+         children = self.children(),
+         calculator = $('<span style="display: inline-block;" />'),
+         height;
+ 
+    var selfHeight = self.height();
+     
+     children.wrap(calculator);
+     height = children.parent().height(); // parent = the calculator wrapper
+     children.unwrap();
+     return height * children.length;
+ };
+
 var MatchConsolePainter = function() {
     "use strict";
     var DisplayDiv = "consoleDiv";
     var text = [];
     var height = $("#" + DisplayDiv).height() - 8;
 
+    var divObject = function() {
+        return $("#" + DisplayDiv);
+    }
+
     this.setText = function(lines) {
         text = lines;
     };
 
     this.draw = function() {
-        $("#" + DisplayDiv).html("");
+        divObject().html("");
         for (var i = 0; i < text.length; i++) {
-            $("#" + DisplayDiv).append(text[i] + "</br>");
+            divObject().append(text[i] + "</br>");
         }
-        var scrollAmount = $("#" + DisplayDiv).height();
-        $("#" + DisplayDiv).scrollTop(scrollAmount);
+        var scrollAmount = divObject().textHeight();
+       divObject().scrollTop(scrollAmount);
     };
 };
 
